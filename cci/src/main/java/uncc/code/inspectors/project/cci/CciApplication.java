d38fdc961907f38018617f873405b2966f8c74be
smahavadi <svmahavadi@gmail.com>
2022-10-20 17:32:11 -0400

    added mongodb driver dependency and fixed controller issues

--- cci/src/main/java/uncc/code/inspectors/project/cci/CciApplication.java
+++ cci/src/main/java/uncc/code/inspectors/project/cci/CciApplication.java
@@ -3,14 +3,14 @@ package uncc.code.inspectors.project.cci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.bson.Document;
+// import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.conversions.Bson;
-import java.util.concurrent.TimeUnit;
+// import java.util.concurrent.TimeUnit;
 import org.bson.Document;
 
 /*
@@ -18,23 +18,23 @@ import org.bson.Document;
  * https://mongodb.github.io/mongo-java-driver
  */
 
-Bson filter = new Document();
-
-MongoClient mongoClient = new MongoClient(
-    new MongoClientURI(
-        "mongodb+srv://itcs4155:YG8LDwo4nWJZh7BG@cluster0.elkihwi.mongodb.net/test"
-    )
-);
-
-MongoDatabase database = mongoClient.getDatabase("fci");
-MongoCollection<Document> collection = database.getCollection("inspectors");
-FindIterable<Document> result = collection.find(filter);
-
 @SpringBootApplication
 public class CciApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CciApplication.class, args);
 	}
+
+		Bson filter = new Document();
+
+		MongoClient mongoClient = new MongoClient(
+			new MongoClientURI(
+				"mongodb+srv://itcs4155:YG8LDwo4nWJZh7BG@cluster0.elkihwi.mongodb.net/test"
+			)
+		);
+
+		MongoDatabase database = mongoClient.getDatabase("fci");
+		MongoCollection<Document> collection = database.getCollection("inspectors");
+		FindIterable<Document> result = collection.find(filter);
 
 }
