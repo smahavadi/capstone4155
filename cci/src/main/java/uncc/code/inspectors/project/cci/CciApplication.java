6aa3e0b72a8f8d71632e0974350eb4fda2fbf938
anguye65 <anguye65@uncc.edu>
2022-10-20 17:08:16 -0400

    Add MongoDB driver

--- cci/src/main/java/uncc/code/inspectors/project/cci/CciApplication.java
+++ cci/src/main/java/uncc/code/inspectors/project/cci/CciApplication.java
@@ -3,6 +3,33 @@ package uncc.code.inspectors.project.cci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.conversions.Bson;
+import java.util.concurrent.TimeUnit;
+import org.bson.Document;
+
+/*
+ * Requires the MongoDB Java Driver.
+ * https://mongodb.github.io/mongo-java-driver
+ */
+
+Bson filter = new Document();
+
+MongoClient mongoClient = new MongoClient(
+    new MongoClientURI(
+        "mongodb+srv://itcs4155:YG8LDwo4nWJZh7BG@cluster0.elkihwi.mongodb.net/test"
+    )
+);
+
+MongoDatabase database = mongoClient.getDatabase("fci");
+MongoCollection<Document> collection = database.getCollection("inspectors");
+FindIterable<Document> result = collection.find(filter);
+
 @SpringBootApplication
 public class CciApplication {
 
